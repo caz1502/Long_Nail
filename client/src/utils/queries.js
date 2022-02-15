06d6cc1887cc2ec9ac22188b07e6e1f580bf0e9c
Carol Shiers <carol.shiers@gmail.com>
2022-02-15 21:46:06 +1000

    updated with client queries, still to be checked but app is still running

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const QUERY_USER = gql`
   query user($username: String!) {
@@ -15,16 +15,108 @@ export const QUERY_USER = gql`
   }
 `;
 
-// added
-export const QUERY_ALL_PRODUCTS = gql`
-  {
-    products {
+export const QUERY_ALL_SERVICE = gql`
+  query getServices {
+    services {
       _id
       name
       description
-      image     
     }
   }
 `;
 
+export const QUERY_SINGLE_SERVICE = gql`
+  query getSingleService($serviceId: ID!) {
+    service(serviceId: $serviceId) {
+      _id
+      name
+      description
+    }
+  }
+`;
 
+export const QUERY_ALL_COLOUR = gql`
+  query getColours {
+    colours {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_SINGLE_COLOUR = gql`
+  query getSingleColour($colourId: ID!) {
+    colour(colourId: $colourId) {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_ALL_SHAPE = gql`
+  query getShapes {
+    shapes {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_SINGLE_SHAPE = gql`
+  query getSingleShape($shapeId: ID!) {
+    shape(shapeId: $shapeId) {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_ALL_ACRYLIC = gql`
+  query getAcrylics {
+    acrylics {
+      _id
+      name
+      price
+    }
+  }
+`;
+
+export const QUERY_SINGLE_ACRYLIC= gql`
+  query getSingleAcrylic($acrylicId: ID!) {
+    acrylic(acrylicId: $acrylicId) {
+      _id
+      name
+      price
+    }
+  }
+`;
+
+export const QUERY_ALL_RELAXATION = gql`
+  query getRelaxations {
+    relaxations {
+      _id
+      name
+      price
+    }
+  }
+`;
+
+export const QUERY_SINGLE_RELAXATION = gql`
+  query getSingleRelaxation($relaxationId: ID!) {
+    relaxation(relaxationId: $relaxationId) {
+      _id
+      name
+      price
+    }
+  }
+`;
+
+export const QUERY_ALL_WAXING = gql`
+  query getWaxings {
+    waxings {
+      _id
+      name
+      price
+    }
+  }
+`;
