916f291a79c3378edfb9b8dbef37dc935634ab1e
Carol Shiers <carol.shiers@gmail.com>
2022-02-21 19:42:17 +1000

    reseeded

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/nails",
+  process.env.MONGODB_URI || "mongodb://localhost/Long_Or_Short_Nails",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
